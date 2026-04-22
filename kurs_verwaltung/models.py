14fcaaeae05611369a5c8845641743c31dba2d2b
Ralf Hamacher <ralf.hamacher@googlemail.com>
2026-04-23 01:21:30 +0200

    korrektur SecKey Kurs/Professor

--- kurs_verwaltung/models.py
+++ kurs_verwaltung/models.py
@@ -6,25 +6,26 @@ from django.db import models
 
 class Semester(models.Model):
     semester_name = models.CharField(max_length=20, unique=True)
+
+class Professor(models.Model):
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)    
     
 class Kurs(models.Model):
     kurs_name = models.CharField(max_length=100)
     semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
 
 class Student(models.Model):
     matrikel_nummer = models.CharField(max_length=20, unique=True)
     last_name = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
-    kurs = models.ForeignKey(Kurs, on_delete=models.CASCADE)
-
-class Professor(models.Model):
-    last_name = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    kurs = models.ForeignKey(Kurs, on_delete=models.CASCADE)
+    kurs = models.ManyToManyField(Kurs)
 
 class Kursbeschreibung(models.Model):
     kurs = models.OneToOneField(Kurs, on_delete=models.CASCADE)
-    beschreibung = models.TextField()    
+    kurs_name = models.CharField(max_length=100)
+    beschreibung = models.TextField()
 
 class Studentenausweis(models.Model):
     ausweis_nummer = models.CharField(max_length=20, unique=True)
