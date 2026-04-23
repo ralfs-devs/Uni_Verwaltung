afcb1ac9d6018928cc5806826846a3634e1ef636
Ralf Hamacher <ralf.hamacher@googlemail.com>
2026-04-23 12:48:39 +0200

    Tabellen als Grafik

--- kurs_verwaltung/models.py
+++ kurs_verwaltung/models.py
@@ -30,4 +30,5 @@ class Kursbeschreibung(models.Model):
 class Studentenausweis(models.Model):
     ausweis_nummer = models.CharField(max_length=20, unique=True)
     student = models.OneToOneField(Student, on_delete=models.CASCADE)
+
        
